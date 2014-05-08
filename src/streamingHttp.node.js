0ab6486612eb0f41f1103eeafb16c8aadd193c50
Eric Fernberg <ericf2023@gmail.com>
2014-05-08 11:42:44 -0400

    change the fetchhttpurl function to include the query string or a url

--- src/streamingHttp.node.js
+++ src/streamingHttp.node.js
@@ -3,13 +3,13 @@ function httpTransport(){
 }
 
 /**
- * A wrapper around the browser XmlHttpRequest object that raises an 
+ * A wrapper around the browser XmlHttpRequest object that raises an
  * event whenever a new part of the response is available.
- * 
- * In older browsers progressive reading is impossible so all the 
+ *
+ * In older browsers progressive reading is impossible so all the
  * content is given in a single call. For newer ones several events
  * should be raised, allowing progressive interpretation of the response.
- *      
+ *
  * @param {Function} oboeBus an event bus local to this Oboe instance
  * @param {XMLHttpRequest} http the http implementation to use as the transport. Under normal
  *          operation, will have been created using httpTransport() above
@@ -19,95 +19,95 @@ function httpTransport(){
  * @param {String} contentSource the url to make a request to, or a stream to read from
  * @param {String|Null} data some content to be sent with the request.
  *                      Only valid if method is POST or PUT.
- * @param {Object} [headers] the http request headers to send                       
- */  
+ * @param {Object} [headers] the http request headers to send
+ */
 function streamingHttp(oboeBus, http, method, contentSource, data, headers) {
    "use strict";
 
    function readStreamToEventBus(readableStream) {
-         
-      // use stream in flowing mode   
+
+      // use stream in flowing mode
       readableStream.on('data', function (chunk) {
-                                             
+
          oboeBus(STREAM_DATA).emit( chunk.toString() );
       });
-      
+
       readableStream.on('end', function() {
-               
+
          oboeBus( STREAM_END ).emit();
       });
    }
-   
+
    function readStreamToEnd(readableStream, callback){
       var content = '';
-   
+
       readableStream.on('data', function (chunk) {
-                                             
+
          content += chunk.toString();
       });
-      
+
       readableStream.on('end', function() {
-               
+
          callback( content );
       });
    }
-   
+
    function fetchHttpUrl( url ) {
       if( !contentSource.match(/http:\/\//) ) {
          contentSource = 'http://' + contentSource;
-      }                           
-                           
-      var parsedUrl = require('url').parse(contentSource); 
-   
+      }
+
+      var parsedUrl = require('url').parse(contentSource);
+
       var req = http.request({
          hostname: parsedUrl.hostname,
-         port: parsedUrl.port, 
-         path: parsedUrl.pathname,
+         port: parsedUrl.port,
+         path: parsedUrl.path,
          method: method,
-         headers: headers 
+         headers: headers
       });
-      
+
       req.on('response', function(res){
          var statusCode = res.statusCode,
              sucessful = String(statusCode)[0] == 2;
-                                                   
-         oboeBus(HTTP_START).emit( res.statusCode, res.headers);                                
-                                
-         if( sucessful ) {          
-               
+
+         oboeBus(HTTP_START).emit( res.statusCode, res.headers);
+
+         if( sucessful ) {
+
             readStreamToEventBus(res)
-            
+
          } else {
             readStreamToEnd(res, function(errorBody){
-               oboeBus(FAIL_EVENT).emit( 
+               oboeBus(FAIL_EVENT).emit(
                   errorReport( statusCode, errorBody )
                );
             });
-         }      
+         }
       });
-      
+
       req.on('error', function(e) {
-         oboeBus(FAIL_EVENT).emit( 
+         oboeBus(FAIL_EVENT).emit(
             errorReport(undefined, undefined, e )
          );
       });
-      
-      oboeBus(ABORTING).on( function(){              
+
+      oboeBus(ABORTING).on( function(){
          req.abort();
       });
-         
+
       if( data ) {
          req.write(data);
       }
-      
-      req.end();         
+
+      req.end();
    }
-   
+
    if( isString(contentSource) ) {
       fetchHttpUrl(contentSource);
    } else {
       // contentsource is a stream
-      readStreamToEventBus(contentSource);   
+      readStreamToEventBus(contentSource);
    }
 
 }
